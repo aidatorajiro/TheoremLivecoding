54ecea38d6734e2b32b5bae5c1d544ea2c86bf17
aidatorajiro <kawarusosu@zoho.com>
2018-05-04 11:58:22 +0900

    initial commit

---
+++ core.js
@@ -0,0 +1,20 @@
+/**
+ * Create A -> B.
+ */
+function create_a_b (a, b) {
+   return (x) => {
+      if (x === a) {
+        return b;
+      } else {
+        throw new Error('type mismatch');
+      }
+   }
+}
+
+/**
+ * proof of (A -> B) -> ((B -> C) -> (A -> C))
+ */
+prop1 = (ab) => ((bc) => ((a) => (bc(ab(a)))))
+
+// the prop (A -> B) -> ((B -> C) -> (A -> C))
+((prop1(create_a_b('A', 'B')))(create_a_b('B', 'C')))("A") === "C" // -> True
