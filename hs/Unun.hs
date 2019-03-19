ff4ea1180f098e80eb05cfd74d4b6552da1bc1d9
aidatorajiro <kawarusosu@zoho.com>
2019-03-19 18:06:55 +0900

    update Unun.hs

--- hs/Unun.hs
+++ hs/Unun.hs
@@ -1,37 +1,37 @@
 data Prop = Var String | Forall String Prop | (:->) Prop Prop deriving (Eq, Show)
 
-data Proof = UnProof Prop deriving (Eq, Show)
+data Proof = Proof Prop deriving (Eq, Show)
 
--- substitute `value` as `var` in `base`
+-- | Substitute `value` as `var` in `base`
 substitute :: String -> Prop -> Prop -> Prop
 substitute var base@(Var s) value = if var == s then value else base
+substitute var base@(Forall s p) value = Forall s (substitute var p value)
+substitute var base@(p :-> q) value = (substitute var p value) :-> (substitute var q value)
 
 dArrow :: Proof -> Proof -> Proof
-dArrow (UnProof (a :-> b)) (UnProof c) = if a == c then UnProof b else error "mismatch at dArrow"
+dArrow (Proof (a :-> b)) (Proof c) = if a == c then Proof b else error "mismatch at dArrow"
 dArrow _ _ = error "mismatch at dArrow"
 
 cArrow :: Prop -> (Proof -> Proof) -> Proof
-cArrow a@(p :-> q) f = if f (UnProof p) == (UnProof q) then UnProof a else error "mismatch at cArrow"
+cArrow a@(p :-> q) f = if f (Proof p) == (Proof q) then Proof a else error "mismatch at cArrow"
 cArrow _ _ = error "mismatch at cArrow"
 
 dForall :: Proof -> Prop -> Proof
-dForall (UnProof (Forall s p)) q = UnProof (substitute s p q)
+dForall (Proof (Forall s p)) q = Proof (substitute s p q)
 dForall _ _ = error "mismatch at dForall"
 
 cForall :: Prop -> Proof -> Proof
-cForall a@(Forall _ p) (UnProof q) = if p == q then UnProof a else error "mismatch at cForall"
+cForall a@(Forall _ p) (Proof q) = if p == q then Proof a else error "mismatch at cForall"
 cForall _ _ = error "mismatch at cForall"
 
 getProp :: Proof -> Prop
-getProp (UnProof p) = p
+getProp (Proof p) = p
 
 thm1 :: Prop
 thm1 = Forall "a" (Var "a" :-> Var "a")
 
 prf1 :: Proof
-prf1 = cArrow thm1 (\a ->
-        cArrow ((getProp a) :-> (getProp a)) (\_ -> a)
-    )
+prf1 = cForall thm1 (cArrow (Var "a" :-> Var "a") (\a -> a))
 
 thm2 :: Prop
 thm2 = 
